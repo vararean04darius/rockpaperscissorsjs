92982988c9734140d63446577494d6153ba977fc
vararean04darius <91600164+vararean04darius@users.noreply.github.com>
2024-04-19 12:39:53 +0300

    Add computer choice function

--- rockpaper.js
+++ rockpaper.js
@@ -1,1 +1,17 @@
 console.log("Hello World!")
+
+function getRandomInt() {
+    return Math.floor(Math.random() * 3);
+  }
+
+function getComputerChoice() {
+    let compchoice =getRandomInt();
+    if (compchoice === 0){
+        return "Rock";
+    } else if (compchoice === 1){
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+console.log(getComputerChoice());
